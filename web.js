b7ecfb60a67f610ce61259e155035fff77c5db36
Supatta Niramarnkarn <sniramarnkarn@gmail.com>
2013-07-11 00:14:33 +0000

    Updated web.js Hw3Part1

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var fs = require('fs');
   var buf = new Buffer(fs.readFileSynch('index.html'),'utf-8');
   response.send(buf.toString());
 });
